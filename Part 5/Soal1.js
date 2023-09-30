2dbd8f9049890402a03e11354f5039d63d340a11
MahesWR <116471087+MahesWR@users.noreply.github.com>
2023-09-30 21:22:23 +0800

    Add files via upload

---
+++ Part 5/Soal1.js
@@ -0,0 +1,15 @@
+console.log("Welcome To Proxytia")
+let nama = "Bonzy"
+let peran = "Kesatria"
+
+if (nama = "") {
+    console.log("Nama Wajib Diisi")
+} else if (peran = "Kesatria") {
+    console.log("halo Ksatria" + nama + ", kamu dapat menyerang dengan senjatamu!")
+} else if (peran = "Tabib") {
+    console.log("halo Tabib" + nama + ", kamu akan membantu temanmu yang terluka")
+} else if (peran = "Penyihir") {
+    console.log("halo Penyihir" + nama + ", ciptakan keajaiban yang membantu kemenanganmu!")
+} else {
+    console.log("tapi kayaknya kamu jadi bot aja ya, peran yang kamu pilih ga ada")
+}
