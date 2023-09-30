7acec70a654da3a2b4fa38205fb11165a5f7177d
MahesWR <116471087+MahesWR@users.noreply.github.com>
2023-09-30 21:09:18 +0800

    Add files via upload

---
+++ Part 5/if-else.js
@@ -0,0 +1,11 @@
+let nilai = 49
+
+console.log(nilai >= 50) // Tergantung Nilai, Hasilnya True or False
+
+if(nilai >= 50) { // True or False
+    console.log("Lulus")
+} else if(nilai >= 40) {
+    console.log("Tidak Lulus")
+} else {
+    console.log("Tidak Lulus Pake Banget")
+}
